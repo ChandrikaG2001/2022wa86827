71c4e316269a058095c0f71b0724d922449c6b13
ChandrikaG2001 <chandrikaguddanti2001@gmail.com>
2026-09-21 16:00:58 +0530

    Update greeting message in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello Jenkins Continuous Integration!");
+        System.out.println("Hello Jenkins Continuous Integration..!");
 
     }
 }
